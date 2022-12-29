dc90cbdaabd2b271f9d3e4f06a42928782f77311
Jaime Nuci <jnuci@ucsb.edu>
2022-12-28 19:57:12 -0800

    Hard code post placeholders. Add Create and Read functionality.

--- main.py
+++ main.py
@@ -1,4 +1,61 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Response, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 # uvicorn main:app --reload
+
+app = FastAPI()
+
+# using the Post class with BaseModel allows us to check
+# for each entry we want or give default values
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+# each post represented as dictionary nested in list
+my_posts = [{"title": "title of post1", "content": "content of post1", "id": 1},
+            {"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == int(id):
+            return p
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+    # title str, content str
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    # we use 'id: int' to validate id can be an int
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} does not exist. Sorry pal.")
+        # can also use
+        # response.status_code = 404
+        # return {"post_detail": f"Post with id {id} does not exist. Sorry :( ."}
+    return {"post_detail": post} 
+
+'''
+Create: Post
+Read: Get
+Update: Put/Patch, put submits all the info again.
+        Patch will change only what you submit
+Delete: Delete
+'''
